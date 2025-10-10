56074695a8dbdd478cef2f0574ffddb32db3f0f4
Michelle Diolola <michelle.diolola@code.berlin>
2025-10-10 11:11:19 +0200

    Migration Test
    Test made

--- database/factories/ArticlesFactory.php
+++ database/factories/ArticlesFactory.php
@@ -3,31 +3,21 @@
 namespace Database\Factories;
 
 use App\Models\Article;
-use App\Models\User;
-Use Illuminate\Database\Eloquent\Fatories\Factory
+Use Illuminate\Database\Eloquent\Fatories\Factory;
 
-class ArticleFactory extends Factory;
+/**
+ * @extends \Illuminate\Database\Eloquent\Factories\Factory<\App\Models\Article>
+ */
+class ArticleFactory extends Factory
 {
     protected $model = Article::class;
-    {
+    
         public function definition(): array
         {
             return [
-                'title' => facke()->sentence(),
-                'content' =>fake()->paragraphs(3, true),
-                'published' => fake()->boolean(),
-                'author_id' => fake()->numberBetween(1,10),
+                'author_id' => // or ->randomDigitNotZero()
+                // add more fields like 'title', => $this->faker->sentence(),
             ];
         }
     }       
-            ]
-        }
-    }
-}
-
-{
-    
-            //
-        ];
-    }
-}
+   
