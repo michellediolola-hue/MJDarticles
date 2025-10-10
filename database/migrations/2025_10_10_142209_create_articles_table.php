c47f544106999d03b018475ba7bf7ca97613484b
Michelle Diolola <michelle.diolola@code.berlin>
2025-10-10 16:24:24 +0200

    migration articles
    articles migration missing

--- database/migrations/2025_10_10_142209_create_articles_table.php
+++ database/migrations/2025_10_10_142209_create_articles_table.php
@@ -6,23 +6,22 @@ use Illuminate\Support\Facades\Schema;
 
 return new class extends Migration
 {
+    /**
+     * Run the migrations.
+     */
     public function up(): void
     {
         Schema::create('articles', function (Blueprint $table) {
             $table->id();
-            $table->string('title');
-            $table->text('content');
             $table->timestamps();
         });
     }
 
+    /**
+     * Reverse the migrations.
+     */
     public function down(): void
     {
         Schema::dropIfExists('articles');
     }
 };
-
-
-    
-
-            
